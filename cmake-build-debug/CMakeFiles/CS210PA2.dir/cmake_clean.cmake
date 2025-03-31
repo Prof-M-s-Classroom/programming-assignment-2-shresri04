45c8b1e861c9da1797e946be8584bc6b96e02aa4
shresri04 <ssridharan6793@sdsu.edu>
2025-03-30 21:39:17 -0700

    Finished Project

---
+++ cmake-build-debug/CMakeFiles/CS210PA2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS210PA2.dir/Story.cpp.obj"
+  "CMakeFiles/CS210PA2.dir/Story.cpp.obj.d"
+  "CMakeFiles/CS210PA2.dir/main.cpp.obj"
+  "CMakeFiles/CS210PA2.dir/main.cpp.obj.d"
+  "CS210PA2.exe"
+  "CS210PA2.exe.manifest"
+  "CS210PA2.pdb"
+  "libCS210PA2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS210PA2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
