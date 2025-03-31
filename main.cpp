555b1477c7c7d6effbdee87a4c535ef1f18db919
shresri04 <ssridharan6793@sdsu.edu>
2025-03-31 00:59:48 -0700

    Merge remote-tracking branch 'origin/main'

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     GameDecisionTree<Story> myGame;
 
-    string filename = "C:/Users/USER 1/CLionProjects/programming-assignment-2-shresri04/story.txt";  // Specify the input file
+    string filename = "story.txt";  // Specify the input file
     char delimiter = ',';                // '|' is the delimiter, change if needed
 
     // Load the story data from the file
